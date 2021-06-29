baa0b49696c6696725548e8824fcbf0127c262d2
Sella Putri Sari <74763171+buludhomba@users.noreply.github.com>
2021-06-29 18:08:51 +0700

    Create Jangkrik.java

---
+++ Polymorphism/Jangkrik.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polymorphism;
+
+/**
+ *
+ * @author Root
+ */
+public class Jangkrik extends Hewan {
+    
+    public void suara(){
+    System.out.println("krikrikrikrikkk");
+    }
+    
+     public void berjalan(){
+    System.out.println("melompat-lompat");
+    System.out.println("*-------------------*");
+    }
+}
