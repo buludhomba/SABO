1c7c0749dd6d2368356731528d41b0f944167c76
Sella Putri Sari <74763171+buludhomba@users.noreply.github.com>
2021-06-29 18:08:36 +0700

    Create Hewan.java

---
+++ Polymorphism/Hewan.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Polymorphism;
+
+/**
+ *
+ * @author Root
+ */
+public class Hewan {
+    //membuat tipe data
+    String jenis;
+    String ciri;
+    
+    //method suara hewan
+    public void suara(){
+    System.out.println("Suara Hewan");
+    }
+    
+     public void berjalan(){
+    System.out.println("Cara berjalan hewan");
+    }
+}
