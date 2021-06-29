2cd046ab48bfe89d25ed52d961babf51319e3125
Sella Putri Sari <74763171+buludhomba@users.noreply.github.com>
2021-06-29 18:13:08 +0700

    Create Kendaraan.java

---
+++ Overriding/Kendaraan.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Root Sella
+ */
+//class Kendaraan merupakan super class
+public class Kendaraan {
+    
+    //overriden method = method yang terdapat di super class
+    void bergerak(){
+  System.out.println("Kendaraan dapat bergerak!");
+   
+}
+    void mempunyai(){
+     System.out.println("Kendaraan mempunyai");
+    }
+}
